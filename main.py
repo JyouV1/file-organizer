270f8dfacd2c37e13ecb725433ccfa4aa6b78b4a
galaxybo <jonathanker975@gmail.com>
2026-05-02 15:26:11 +0700

    make the script create Tugas Sekolah folder and mapel subfolder

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import os
 import shutil
+from pathlib import Path
 
-def 
+home = Path.home()
+Documents = home / "Documents"
+
+target = Documents / "Tugas Sekolah"
+
+folders = ["b_ind", "b_ing", "b_man", "dtjkt", "informatika", "mtk", "agama", "ppkn", "ipas", "sejarah", "seni_budaya", "literasi"]
+
+try:
+    target.mkdir(parents=True, exist_ok=False)
+    print("create Tugas Sekolah directory")
+except FileExistsError:
+    print("directory Tugas Sekolah already created")
+
+for mapel in folders:
+    (target / mapel).mkdir(exist_ok=True)
+
