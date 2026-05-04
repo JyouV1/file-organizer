230618c177315c138bdba528b8de4c0a132dfb47
galaxybo <jonathanker975@gmail.com>
2026-05-04 20:51:25 +0700

    change from script to organize school task file, to organize by their extension

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ from pathlib import Path
 home = Path.home()
 Documents = home / "Documents"
 
-target = Documents / "Tugas Sekolah"
 
-folders = ["b_ind", "b_ing", "b_man", "dtjkt", "informatika", "mtk", "agama", "ppkn", "ipas", "sejarah", "seni_budaya", "literasi"]
+folders = ["txt", "docx", "ppt", "pdf", "exel"]
+
 
 try:
-    target.mkdir(parents=True, exist_ok=False)
-    print("create Tugas Sekolah directory")
+    Documents.mkdir(parents=True, exist_ok=False)
+    print("create extension folder")
 except FileExistsError:
-    print("directory Tugas Sekolah already created")
+    print("extension folder already created")
 
-for mapel in folders:
-    (target / mapel).mkdir(exist_ok=True)
-
+for extension in folders :
+    ext = Documents / extension
+    ext.mkdir(parents=True, exist_ok=True)
