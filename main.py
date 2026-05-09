48b37862093a0f7b0d4247b9d9a9bd45818cc8e8
galaxybo <jonathanker975@gmail.com>
2026-05-09 16:47:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,27 +2,29 @@ import os
 import shutil
 from pathlib import Path
 
-Documents = Path.home() / "Documents"
+def main():
+    Documents = Path.home() / "Documents"
 
 
-extensionlist = {
-    "txt" : [".txt"],
-    "docx" : [".docx"],
-    "ppt" : [".pptx"],
-    "pdf" : [".pdf"],
-    "exel" : [".xlsx"]
-}
+    extensionlist = {
+        "txt" : [".txt"],
+        "docx" : [".docx"],
+        "ppt" : [".pptx"],
+        "pdf" : [".pdf"],
+        "exel" : [".xlsx"]
+    }
 
 
 
-for folder in extensionlist :
-    os.makedirs(os.path.join(Documents, folder), exist_ok=True)
+    for folder in extensionlist :
+        os.makedirs(os.path.join(Documents, folder), exist_ok=True)
 
-for file in os.listdir(Documents):
-    file_path = os.path.join(Documents, file)
-    if os.path.isfile(file_path):
-        for folder, extensions in extensionlist.items():
-            if any(file.endswith(ext) for ext in extensions):
-                shutil.move(file_path, os.path.join(Documents, folder, file))
-                break
+    for file in os.listdir(Documents):
+        file_path = os.path.join(Documents, file)
+        if os.path.isfile(file_path):
+            for folder, extensions in extensionlist.items():
+                if any(file.endswith(ext) for ext in extensions):
+                    shutil.move(file_path, os.path.join(Documents, folder, file))
+                    break
 
+main()
